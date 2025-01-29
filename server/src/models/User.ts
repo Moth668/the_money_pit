78cdc44e9f000bc6d3abbe959c70b17292d32420
Angelica Mora <angiethelibrarian@gmail.com>
2025-01-28 21:42:12 -0500

    pushing server side files and will work on Client side files

---
+++ server/src/models/User.ts
@@ -0,0 +1,45 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+interface IUser extends Document {
+    email: string;
+    password: string;
+
+}
+
+const userSchema = new Schema<IUser>({
+    email: { 
+        type: String, 
+        required: true, 
+        unique: true 
+    },
+
+    password: { 
+        type: String, 
+        required: true 
+    },
+    // set this to use virtual below
+    {
+        toJSON: {
+          virtuals: true,
+        },
+      }
+});
+
+// hash user password
+userSchema.pre<IUser>('save', async function (next) {
+    if (this.isNew || this.isModified('password')) {
+      const saltRounds = 10;
+      this.password = await bcrypt.hash(this.password, saltRounds);
+    }
+  
+    next();
+  });
+
+  // custom method to compare and validate password for logging in
+  userSchema.methods.isCorrectPassword = async function (password: string): Promise<boolean> {
+    return await bcrypt.compare(password, this.password);
+  };
+
+const User = mongoose.model<IUser>("User", userSchema);
+export default User;
+  
