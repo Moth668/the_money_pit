3febb83b680a352199f06905a2fc5ef371bc9ba7
Angelica Mora <angiethelibrarian@gmail.com>
2025-01-28 21:43:42 -0500

    did not save server ts file

--- server/src/models/User.ts
+++ server/src/models/User.ts
@@ -7,39 +7,38 @@ interface IUser extends Document {
 }
 
 const userSchema = new Schema<IUser>({
-    email: { 
-        type: String, 
-        required: true, 
-        unique: true 
+    email: {
+        type: String,
+        required: true,
+        unique: true
     },
 
-    password: { 
-        type: String, 
-        required: true 
+    password: {
+        type: String,
+        required: true
     },
     // set this to use virtual below
     {
         toJSON: {
-          virtuals: true,
+            virtuals: true,
         },
-      }
+    }
 });
 
 // hash user password
 userSchema.pre<IUser>('save', async function (next) {
     if (this.isNew || this.isModified('password')) {
-      const saltRounds = 10;
-      this.password = await bcrypt.hash(this.password, saltRounds);
+        const saltRounds = 10;
+        this.password = await bcrypt.hash(this.password, saltRounds);
     }
-  
-    next();
-  });
 
-  // custom method to compare and validate password for logging in
-  userSchema.methods.isCorrectPassword = async function (password: string): Promise<boolean> {
+    next();
+});
+
+// custom method to compare and validate password for logging in
+userSchema.methods.isCorrectPassword = async function (password: string): Promise<boolean> {
     return await bcrypt.compare(password, this.password);
-  };
+};
 
 const User = mongoose.model<IUser>("User", userSchema);
 export default User;
-  
