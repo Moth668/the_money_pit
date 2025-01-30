409caac2febb8e426c804b28aab3c765db59adf3
Anthony Gillespie <unknownkonflikt@gmail.com>
2025-01-30 12:04:46 -0600

    added type defs and queries

--- client/src/utils/queries.ts
+++ client/src/utils/queries.ts
@@ -1,9 +1,73 @@
 import { gql } from '@apollo/client';
 
-export const GET_CURRENT_SAVINGS = gql``
 
-export const GET_MONTHLY_INCOME = gql``
 
-export const GET_MONTHLY_EXPENSES = gql``
+export const GET_USER_DETAILS = gql`
+query GET_USER_DETAILS($id: ID!) {
+    user(id: $id) {
+        name
+        Income {
+            month
+            income
+            date
+        }
+        Expenses {
+            month
+            expense
+            date
+        }
+        Savings {
+            month
+            savings
+            date
+        }
+        Investments {
+            month
+            investment
+            date
+        }
+    }
+}`
+export const GET_CURRENT_SAVINGS = gql`
+query GET_CURRENT_SAVINGS($id: ID!) {
+    user(id: $id) {
+        Savings {
+            month
+            savings
+            date
+        }
+    }
+}`
 
-export const GET_INVESTMENT_BALANCE = gql``
+export const GET_MONTHLY_INCOME = gql`
+query GET_MONTHLY_INCOME($id: ID!) {
+    user(id: $id) {
+        Income {
+            month
+            income
+            date
+        }
+    }
+}`
+
+export const GET_MONTHLY_EXPENSES = gql`
+query GET_MONTHLY_EXPENSES($id: ID!) {
+    user(id: $id) {
+        Expenses {
+            month
+            expense
+            date
+        }
+    }
+}`
+
+export const GET_INVESTMENT_BALANCE = gql`
+query GET_INVESTMENT_BALANCE($id: ID!) {
+    user(id: $id) {
+        Investments {
+            month
+            investment
+            date
+        }
+    }
+}`;
--- server/src/schemas/typeDefs.ts
+++ server/src/schemas/typeDefs.ts
@@ -1,7 +1,35 @@
-const typeDefs = `
+const user = `
   type Query {
-    hello: String
+    name: String
+    monthlyIncome: [Income]
+    monthlyExpenses: [Expense]
+    currentSavings: [Savings]
+    currentInvestments: [Investment]
+  }
+
+  type Income {
+    month: String
+    income: Float
+    date: String
+    }
+
+  type Expense {
+    month: String
+    expense: Float
+    date: String
+  }
+
+  type Savings {
+    month: String
+    savings: Float
+    date: String
+  }
+
+  type Investment {
+    month: String
+    investment: Float
+    date: String
   }
 `;
 
-export default typeDefs;
+export default user;
