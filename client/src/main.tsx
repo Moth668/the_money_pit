e11d9f99dad620c0a3dcd9bdc203015f75350dab
OKenne <175995429+O-KenneDevWorks@users.noreply.github.com>
2025-01-29 21:11:01 -0500

    Set up BrowserRouter

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,10 +1,31 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Dashboard from "./pages/Dashboard";
+import MonthlyIncome from "./pages/MonthlyIncome";
+import MonthlyExpenses from "./pages/MonthlyExpenses";
+import CurrentSavings from "./pages/CurrentSavings";
+import InvestmentBalance from "./pages/InvestmentBalance";
+import Home from "./pages/Home"
+import App from './app2.tsx'
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+console.log("Main.tsx is executing...");
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    // errorElement: <ErrorPage />,
+    children: [
+      { index: true, element: <Dashboard /> },
+      { path: 'monthly-income', element: <MonthlyIncome /> },
+      { path: 'monthly-expenses', element: <MonthlyExpenses /> },
+      { path: 'current-savings', element: <CurrentSavings /> },
+      { path: 'investment-balance', element: <InvestmentBalance /> },
+    ],
+  },
+]);
+
+const rootElement = document.getElementById('root');
+if (rootElement) {
+  ReactDOM.createRoot(rootElement).render(<RouterProvider router={router} />);
+}
