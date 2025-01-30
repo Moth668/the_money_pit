2feb2780d1b6df30985ab1fc14d101a878c8fbde
OKenne <175995429+O-KenneDevWorks@users.noreply.github.com>
2025-01-29 21:11:22 -0500

    Added blank queries for linting purposes

---
+++ client/src/utils/queries.ts
@@ -0,0 +1,9 @@
+import { gql } from '@apollo/client';
+
+export const GET_CURRENT_SAVINGS = gql``
+
+export const GET_MONTHLY_INCOME = gql``
+
+export const GET_MONTHLY_EXPENSES = gql``
+
+export const GET_INVESTMENT_BALANCE = gql``
