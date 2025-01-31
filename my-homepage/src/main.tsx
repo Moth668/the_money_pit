05043b7c9fb518d8405647061f1c612c679ffba3
w3schools--test <66185683+Gradius64@users.noreply.github.com>
2025-01-30 19:41:43 -0600

    added all the things!!!!!

--- my-homepage/src/main.tsx
+++ my-homepage/src/main.tsx
@@ -1,11 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import App from './App.tsx'
-import MyForm from './userinput/myform'
 import './index.css'
+import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <MyForm />
+    <App />
   </StrictMode>,
 )
