3ba2f343f8ee7472a4227e775707ba1b0d2993f6
OKenne <175995429+O-KenneDevWorks@users.noreply.github.com>
2025-01-30 20:47:23 -0500

    Moved route to main from app.tsx

--- client/src/app2.tsx
+++ client/src/app2.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import { Outlet, Routes, useLocation } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import { Provider } from "@/components/ui/provider";
-import { Route } from 'react-router-dom';
-import Home from "./pages/Home";
-import MonthlyIncome from "./pages/MonthlyIncome";
-import MonthlyExpenses from "./pages/MonthlyExpenses";
-import CurrentSavings from "./pages/CurrentSavings";
-import InvestmentBalance from "./pages/InvestmentBalance";
+// import { Route } from 'react-router-dom';
+// import Home from "./pages/Home";
+// import MonthlyIncome from "./pages/MonthlyIncome";
+// import MonthlyExpenses from "./pages/MonthlyExpenses";
+// import CurrentSavings from "./pages/CurrentSavings";
+// import InvestmentBalance from "./pages/InvestmentBalance";
 
 
 // Debug Component to Log Current Location
@@ -22,13 +22,13 @@ const App: React.FC = () => {
       <Provider>
         <h1>The Money Pit</h1>
         <DebugLocation />
-        <Routes>
+        {/* <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/monthly-income" element={<MonthlyIncome />} />
         <Route path="/monthly-expenses" element={<MonthlyExpenses />} />
         <Route path="/current-savings" element={<CurrentSavings />} />
         <Route path="/investment-balance" element={<InvestmentBalance />} />
-        </Routes>
+        </Routes> */}
         <Outlet />
       </Provider>
   );
