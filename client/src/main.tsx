f89ddf91637d3e5deaee43769a51031009b60471
OKenne <175995429+O-KenneDevWorks@users.noreply.github.com>
2025-02-03 20:01:20 -0500

    Added page for viewprofile

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -8,6 +8,7 @@ import InvestmentBalance from "./pages/InvestmentBalance";
 import Home from "./pages/Home"
 import App from './app2'
 import LoginForm from "./components/LoginForm";
+import ViewProfile from "./components/ViewProfile";
 
 console.log("Main.tsx is executing...");
 
@@ -22,6 +23,7 @@ const router = createBrowserRouter([
       { path: 'CurrentSavings', element: <CurrentSavings /> },
       { path: 'InvestmentBalance', element: <InvestmentBalance /> },
       { path: 'LoginForm', element: <LoginForm /> },
+      { path: "ViewProfileCard", element: <ViewProfile /> },
     ],
   },
 ]);
