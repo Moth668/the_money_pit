dd95a9daf8015ab51c861113afc1f3b33b2ce011
OKenne <175995429+O-KenneDevWorks@users.noreply.github.com>
2025-02-05 19:01:25 -0500

    Fixed issue with header image scaling

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -5,24 +5,23 @@ import bunkerPig from '../../../assets/bunker-pig.jpg';
 const Header: React.FC = () => {
     return (
         <Box as="header" bg="gray.800" p={4} height={{ base: '150px', sm: '200px', md: '250px' }}>
-            {/* Responsive height based on screen size */}
             <Flex align="center" justify="center" height="100%">
                 <Link to="/">
                     <Box
-                        width="100%"
-                        height="100%"
+                        display="flex"
+                        justifyContent="center"
+                        alignItems="center"
+                        width= "100%"
+                        height={{ base: "100px", sm: "200px", md: "250px" }} // Fixed height
                         overflow="hidden"
                         border="2px solid black"
                     >
                         <Image
                             src={bunkerPig}
                             alt="Logo"
-                            width="50%" // Makes the image fill the width of the container
-                            height="50%" // Makes the image fill the height of the container
-                            objectFit="contain" // Ensures the image covers the entire container
-                            maxWidth="100%" // Ensures the image does not exceed its original size
-                            maxHeight="100%" // Ensures the image does not exceed its original size
-                            margin="auto" // Centers the image horizontally
+                            maxWidth="100%" // Keeps image within the container width
+                            maxHeight="100%" // Keeps image within the container height
+                            objectFit="contain" // Ensures the image does not distort
                         />
                     </Box>
                 </Link>
